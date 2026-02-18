92fb6230ce108d47ee1304b1782d7f0740b3cbb6
= <ngstafford1@gmail.com>
2026-02-18 18:11:45 -0500

    planned out clone detection

--- src/clone_detection.hpp
+++ src/clone_detection.hpp
@@ -13,8 +13,21 @@ namespace srcmove {
 
 bool is_clone(move_candidate original, move_candidate modified);
 
-bool is_clone_type_1(move_candidate original, move_candidate modified);
-bool is_clone_type_2(move_candidate original, move_candidate modified);
+bool is_clone_type_1(move_candidate original, move_candidate modified) {
+  // normalize source text. (remove comments/whitespace)
+  // split into lines or blocks
+  // hash or compare strings
+
+  // ideas
+  // line by line hashing
+  // sliding window hashing
+  // suffix trees / suffix arrays
+  return true;
+}
+bool is_clone_type_2(move_candidate original, move_candidate modified) {
+  // tokanize parameters
+  return is_clone_type_1(original, modified);
+}
 bool is_clone_type_3(move_candidate original, move_candidate modified);
 bool is_clone_type_4(move_candidate original, move_candidate modified);
 
