f2ba4508f3d7f7a4256f915105904bc145fa72aa
= <ngstafford1@gmail.com>
2026-02-20 18:27:35 -0500

    rewrite with tag stack

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
  * This file is part of the srcDiff Infrastructure.
  */
 #include <cctype>
+#include <csetjmp>
 #include <iostream>
 #include <string>
 
@@ -20,36 +21,29 @@
 #include "debug.cpp"
 #include "srcml_node.hpp"
 #include "srcml_reader.hpp"
-#include "xpath_bulder.hpp"
+// #include "xpath_bulder.hpp"
 
 namespace srcmove {
 
-struct region {
-  enum class kind { insert, del };
-  kind k;
-  std::size_t start_idx;
-  std::size_t end_idx;
-  std::string file; // from unit@filename
-
-  std::string start_xpath;
-  std::string end_xpath;
+class region {
+public:
+  std::string filename; // from unit@filename
+  std::string xpath;
+  std::string full_name;     // full_name()
+  std::size_t sibling_index; // 1-based for siblings with same name under parent
+  std::size_t start_index;
+  // used to calculate xpath for each sibling node inside this region
+private:
+  std::unordered_map<std::string, std::size_t> child_counts;
 };
 
 // first pass to get information about the nodes. Return information about
 // important nodes
 std::vector<region> collect_regions(srcml_reader &reader) {
-  std::vector<region> out;
+
   std::string current_file;
-
-  int insert_depth = 0;
-  int delete_depth = 0;
-
-  std::vector<std::size_t> insert_starts;
-  std::vector<std::size_t> delete_starts;
-
-  std::vector<std::string> insert_start_xpaths;
-  std::vector<std::string> delete_start_xpaths;
-  xpath_builder xb;
+  std::vector<region> out;
+  std::vector<region> tag_stack;
 
   std::size_t i = 0;
   for (const srcml_node &node : reader) {
@@ -58,89 +52,52 @@ std::vector<region> collect_regions(srcml_reader &reader) {
     // update xpath builder for START first (so current_xpath includes this
     // element)
     if (node.is_start()) {
-      xb.on_node(node);
 
-      // if we are at the start of a new page
+      // check if we are at the start of a new page
       if (node.name == "unit") {
         if (const std::string *f = node.get_attribute_value("filename"))
           current_file = *f;
         else
           current_file.clear(); // not all <unit> tags are for files.
       }
-
-      // START insert
-      else if (node.full_name() == "diff:insert") {
-        assert(delete_depth == 0 && "insert inside delete (not handled yet?)");
-
-        insert_depth++;
-        insert_starts.push_back(i);
-        insert_start_xpaths.push_back(xb.current_xpath());
+      region current;
+      current.full_name = node.full_name();
+      current.filename = current_file;
+      current.start_index = 0;
+      if (tag_stack.empty()) {
+        current.xpath = "/";
+        current.sibling_index = 0;
+      } else {
+        region &parent = tag_stack.back();
+        current.xpath = parent.xpath + "/" + current.full_name;
       }
-
-      // START delete
-      else if (node.full_name() == "diff:delete") {
-        assert(insert_depth == 0 && "delete inside insert (not handled yet?)");
-
-        delete_depth++;
-        delete_starts.push_back(i);
-        delete_start_xpaths.push_back(xb.current_xpath());
-      }
+      tag_stack.push_back(current);
+      std::cout << "added to tag_stack: " << node << "\n";
+      // toodo implement child_counts
     }
 
-    // For END nodes, snapshot BEFORE popping so xpath still points to the
-    // element closing
     if (node.is_end()) {
-      if (node.full_name() == "diff:insert") {
-        assert(insert_depth > 0 && "diff:insert end without start");
-
-        std::string end_xpath =
-            xb.current_xpath(); // currently points to diff:insert[...]
-
-        std::size_t start = insert_starts.back();
-        insert_starts.pop_back();
-        std::string start_xpath = insert_start_xpaths.back();
-        insert_start_xpaths.pop_back();
-        insert_depth--;
-
-        out.push_back(region{region::kind::insert, start, i, current_file,
-                             start_xpath, end_xpath});
+      assert((!tag_stack.empty()) && "tag stack is empty on end tag.");
+      region &top = tag_stack.back();
+      std::cout << "popping from tag_stack: " << top.full_name << "\n";
+      if (top.full_name == "diff:insert" || top.full_name == "diff:delete") {
+        out.push_back(top);
+        std::cout << "pushed to out: " << node << "\n";
       }
-
-      else if (node.full_name() == "diff:delete") {
-        assert(delete_depth > 0 && "diff:delete end without start");
-
-        std::string end_xpath = xb.current_xpath();
-
-        std::size_t start = delete_starts.back();
-        delete_starts.pop_back();
-        std::string start_xpath = delete_start_xpaths.back();
-        delete_start_xpaths.pop_back();
-        delete_depth--;
-
-        out.push_back(region{region::kind::del, start, i, current_file,
-                             start_xpath, end_xpath});
-      }
-
-      // now pop xpath builder on END
-      xb.on_node(node);
+      tag_stack.pop_back();
     }
     ++i;
   }
-
-  assert(insert_depth == 0 && delete_depth == 0);
   return out;
 }
 
 void first_pass(srcml_reader &reader) {
 
   auto regions = collect_regions(reader);
-  for (auto &r : regions) {
-    std::cout << (r.k == region::kind::insert ? "INS" : "DEL") << " ["
-              << r.start_idx << "," << r.end_idx << "] " << r.file << "\n";
-  }
-  // std::size_t i = 0;
-  // for (const srcml_node &node : reader) {
-  //   print_node(node, i++);
+  // for (auto &r : regions) {
+  //   std::cout << (r.k == region::kind::insert ? "INS" : "DEL") << " ["
+  //             << r.start_idx << "," << r.end_idx << "] " << r.filename <<
+  //             "\n";
   // }
 }
 
