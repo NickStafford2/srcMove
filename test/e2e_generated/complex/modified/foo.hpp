79ae6e14fae660aa13610c4251f30b07c28a3c9e
= <ngstafford1@gmail.com>
2026-06-14 16:40:13 -0400

    updated comment of test file to reflect that it moves comments to new files.

--- test/e2e_generated/complex/modified/foo.hpp
+++ test/e2e_generated/complex/modified/foo.hpp
@@ -1,5 +1,3 @@
-// This currently can not be captured because srcdiff deletes the function tag
-// and not the block.
 int changed_function();
 
 char unchanged_function();
--- test/e2e_generated/complex/modified/main.cpp
+++ test/e2e_generated/complex/modified/main.cpp
@@ -1,5 +1,7 @@
 #import "foo.hpp"
 
+// This comment moves to a new file.
+
 int main(int argc, char **argv) {
   int  a = changed_function();
   char c = unchanged_function();
